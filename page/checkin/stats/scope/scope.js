53a4c4d02f630bcdb59f05ed65da519845093bed
steven <steven@nenyah.com>
2020-04-08 23:58:04 +0800

    add date picker for stats

--- page/checkin/stats/scope/scope.js
+++ page/checkin/stats/scope/scope.js
@@ -1,9 +1,30 @@
+import { formatDate } from "/util/utils.js";
 Component({
   mixins: [],
-  data: {},
+  data: {
+    today: "2020-04-01"
+  },
   props: {},
-  didMount() {},
+  didMount() {
+    const date = new Date();
+    const today = formatDate(date, "-");
+    this.setData({
+      today
+    });
+  },
   didUpdate() {},
   didUnmount() {},
-  methods: {},
+  methods: {
+    chooseDate() {
+      dd.datePicker({
+        format: "yyyy-MM-dd",
+        currentDate: this.data.today,
+        success: res => {
+          dd.alert({
+            content: res.date
+          });
+        }
+      });
+    }
+  }
 });
