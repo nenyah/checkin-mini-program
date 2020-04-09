ebc6cf877328ed8fe693ef1b9855acee4e15f535
steven <steven@nenyah.com>
2020-04-09 10:55:43 +0800

    finish checkin-submit page layout

--- page/checkin/checkin-submit/checkin-submit.js
+++ page/checkin/checkin-submit/checkin-submit.js
@@ -1,4 +1,16 @@
 Page({
   data: {},
   onLoad() {},
+  useCamera() {
+    dd.chooseImage({
+      count: 1,
+      sourceType:['camera'],
+      success: res => {
+        dd.alert({
+          title: "选中的图片",
+          content: JSON.stringify(res.filePaths)
+        });
+      }
+    });
+  }
 });
