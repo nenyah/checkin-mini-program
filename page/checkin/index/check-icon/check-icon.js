49359a1611dae76a68745e135fd94b1ae7531aeb
steven <steven@nenyah.com>
2020-04-13 20:31:13 +0800

    realise func limit to have to select visit person

--- page/checkin/index/check-icon/check-icon.js
+++ page/checkin/index/check-icon/check-icon.js
@@ -1,21 +1,25 @@
 import { formatDate } from "/util/utils.js";
 Component({
   mixins: [],
-  data: {
-  },
+  data: {},
   props: {
-    visitsPerson:"",
+    visitsPerson: "",
     ctime: "00:00"
   },
-  didMount() {
-  },
+  didMount() {},
   didUpdate() {},
   didUnmount() {},
   methods: {
-    goSubmit(){
-      my.navigateTo({
-        url: '../checkin-submit/checkin-submit?visitsPerson='+this.props.visitsPerson
-      });
+    goSubmit() {
+      if (this.props.visitsPerson) {
+        my.navigateTo({
+          url:
+            "../checkin-submit/checkin-submit?visitsPerson=" +
+            this.props.visitsPerson
+        });
+      } else {
+        my.alert({ title: "请选择拜访对象！" });
+      }
     }
   }
 });
