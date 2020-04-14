967f58c8928f4d037922cb69d84498a78ca38561
steven <steven@nenyah.com>
2020-04-14 16:17:34 +0800

    finish profile layout

--- page/checkin/profile/profile.js
+++ page/checkin/profile/profile.js
@@ -1,4 +1,61 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    month: "2020-04",
+    userInfo: {
+      avtarImg: "/assets/images/pan_avatar.jpg",
+      name: "潘燕燕",
+      monthCheckinTimes: 5,
+      company: "华东宁波医药有限公司"
+    },
+    items: [
+      {
+        checkIndate: "4月14日",
+        ctime: "10:46",
+        title: "宁波市公安局北仑分局交通警察大队大碶中队",
+        address: "浙江省宁波市北仑区大碶街道大浦河北路",
+        remark: "",
+        visitsPerons: "华东宁波医药有限公司",
+        picUrls: [
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg"
+        ],
+        location: [121.796925, 29.903595]
+      },
+      {
+        checkIndate: "4月10日",
+        ctime: "10:46",
+        title: "宁波市公安局北仑分局交通警察大队大碶中队",
+        address: "浙江省宁波市北仑区大碶街道大浦河北路",
+        remark:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aperiam, mollitia repudiandae alias officiis aliquid aliquam repellat, voluptas necessitatibus rerum quae, explicabo unde accusantium fugit vel debitis! Est, nemo quos!",
+        visitsPerons: "华东宁波医药有限公司",
+        picUrls: [],
+        location: [121.796925, 29.903595]
+      },
+      {
+        checkIndate: "4月14日",
+        ctime: "11:46",
+        title: "宁波市公安局北仑分局交通警察大队大碶中队",
+        address: "浙江省宁波市北仑区大碶街道大浦河北路",
+        remark:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aperiam, mollitia repudiandae alias officiis aliquid aliquam repellat, voluptas necessitatibus rerum quae, explicabo unde accusantium fugit vel debitis! Est, nemo quos!",
+        visitsPerons: "华东宁波医药有限公司",
+        picUrls: [
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg",
+          "/image/fake1.jpg"
+        ],
+        location: [121.796925, 29.903595]
+      }
+    ]
+  },
+  onLoad() {}
 });
