86216b8bd035d659a3e5abf325d1cadf6395986c
steven <steven@nenyah.com>
2020-05-06 09:14:47 +0800

    fix stats bug

--- page/checkin/stats/stats.js
+++ page/checkin/stats/stats.js
@@ -1,77 +1,7 @@
 import { getRecord, getOwnDeptRecord } from "/service/record";
 import { getStorageSync } from "/service/storage";
 import { getDeptInfo } from "/service/dept";
-// 有签到信息
-const items = {
-  checkin: [
-    {
-      name: "陈士明",
-      workcode: "NB1859",
-      lastTime: "08:58",
-      checkInTimes: 3,
-      remark: "test1",
-      address: "宁波市公安局北仑分局交通警察大队",
-      picUrl: "/assets/images/fake1.jpg",
-    },
-    {
-      name: "李丽",
-      workcode: "NB1491",
-      lastTime: "09:58",
-      checkInTimes: 4,
-      address: "宁波市公安局北仑分局交通警察大队",
-    },
-    {
-      name: "邬顶立",
-      workcode: "NB1467",
-      lastTime: "10:58",
-      checkInTimes: 1,
-      address: "宁波市公安局北仑分局交通警察大队",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕",
-      workcode: "NB1428",
-      lastTime: "09:53",
-      checkInTimes: 1,
-      address: "宁波市公安局北仑分局交通警察大队",
-    },
-  ],
-  uncheckin: [
-    {
-      name: "陈士明",
-    },
-    {
-      name: "李丽",
-    },
-    {
-      name: "邬顶立",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕1",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕2",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕3",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕4",
-    },
-    {
-      thumbAvatar: "/assets/images/pan_avatar.jpg",
-      name: "潘燕燕5",
-    },
-  ],
-};
+
 var app = getApp();
 Page({
   data: {
@@ -80,10 +10,10 @@ Page({
       { title: "7", subTitle: "未签到" },
     ],
     activeTab: 0,
-    items: items,
+    items: [],
     dept: "",
     userNum: 0,
-    userids: [],
+    userIds: [],
     date: "",
   },
   onLoad() {
@@ -101,7 +31,11 @@ Page({
     this.setData({
       date: data,
     });
-    this._getRecord({ userIds: this.data.userids, date: data });
+    if (this.data.userIds.length) {
+      this._getRecord({ userIds: this.data.userIds, date: data });
+    } else {
+      this._getOwnDeptRecord({ date: data });
+    }
   },
   onGetNewUser(users) {
     console.log("统计页获得人员数据", users);
