e7c23f9ac955ff33f8ddc74f03d2e0e078e970a9
steven <steven@nenyah.com>
2020-05-20 15:38:36 +0800

    add pulldown request data

--- page/checkin/stats/person-cards/person-cards.js
+++ page/checkin/stats/person-cards/person-cards.js
@@ -4,22 +4,25 @@ Component({
   props: {
     items: [],
     activeIndex: 1,
+    onLower: ()=>{},
   },
-  didMount() {
-  },
-  didUpdate() {
-  },
+  didMount() {},
+  didUpdate() {},
   didUnmount() {},
   methods: {
+    lower() {
+      // console.log("组件触发");
+      this.props.onLower();
+    },
     goToPorfile(e) {
-      console.log('跳转我的页面',e);
+      // console.log("跳转我的页面", e);
       const item = e.currentTarget.dataset.item;
       my.navigateTo({
         url: `/page/checkin/profile/profile?userid=${item.jobNumber}&username=${item.userName}`,
       });
     },
     previewImg(e) {
-      console.log("预览图片", e);
+      // console.log("预览图片", e);
       my.previewImage({
         urls: [e.currentTarget.dataset.src],
       });
--- page/checkin/stats/stats.js
+++ page/checkin/stats/stats.js
@@ -15,14 +15,38 @@ Page({
     userNum: 0,
     userIds: [],
     date: "",
+    pages: 0,
+    current: 0,
+    size: 50,
   },
   onLoad() {
-    //
+    // 首次进入显示本部门信息
     this.setData({
       date: moment(app.globalData.currentTime).format("YYYY-MM-DD"),
     });
     this._getDeptInfo();
-    this._getOwnDeptRecord();
+    this._getOwnDeptRecord({ current: 1, size: 50 });
+  },
+  onLower() {
+    // 下拉时加载更多
+    // console.log("统计页向下");
+    const pages = this.data.pages,
+      date = this.data.date,
+      userIds = this.data.userIds,
+      size = this.data.size;
+    let current = this.data.current;
+
+    if (this.data.userNum > 0) {
+      if (pages > current) {
+        current += current;
+        this._getRecord({ userIds, date, current, size });
+      }
+    } else {
+      if (pages > current) {
+        current += current;
+        this._getOwnDeptRecord({ current, size });
+      }
+    }
   },
   onTabClick(e) {
     this.setData({
@@ -33,8 +57,10 @@ Page({
     console.log("统计页获得日期数据", data);
     this.setData({
       date: data,
+      current: 0,
+      pages: 0,
     });
-    if (this.data.userIds.length) {
+    if (this.data.userNum > 0) {
       this._getRecord({ userIds: this.data.userIds, date: data });
     } else {
       this._getOwnDeptRecord({ date: data });
@@ -49,6 +75,8 @@ Page({
     this.setData({
       userIds: myusers,
       userNum,
+      current: 0,
+      pages: 0,
     });
     this._getRecord({ userIds: myusers, date: this.data.date });
   },
@@ -64,14 +92,7 @@ Page({
     getOwnDeptRecord({ ...opt })
       .then((res) => {
         console.log("获取本部门签到信息", res);
-        const checkinNums = res.signInQty;
-        const uncheckinNums = res.notSignInList.length;
-        const items = res;
-        this.setData({
-          "tabs[0].title": checkinNums,
-          "tabs[1].title": uncheckinNums,
-          items,
-        });
+        this._renderData(res);
       })
       .catch((err) => console.error(err));
   },
@@ -79,14 +100,7 @@ Page({
     getRecord({ ...opt })
       .then((res) => {
         console.log("获取选择人员签到信息", res);
-        const checkinNums = res.signInQty;
-        const uncheckinNums = res.notSignInList.length;
-        const items = res;
-        this.setData({
-          "tabs[0].title": checkinNums,
-          "tabs[1].title": uncheckinNums,
-          items,
-        });
+        this._renderData(res);
       })
       .catch((err) => console.error(err));
   },
@@ -100,5 +114,19 @@ Page({
         });
       })
       .catch((err) => console.error(err));
+  },
+  _renderData(res) {
+    const checkinNums = res.signInQty,
+      uncheckinNums = res.notSignInList.length,
+      pages = res.signInHisPage.pages,
+      current = res.signInHisPage.current,
+      items = res;
+    this.setData({
+      "tabs[0].title": checkinNums,
+      "tabs[1].title": uncheckinNums,
+      pages,
+      current,
+      items,
+    });
   },
 });
